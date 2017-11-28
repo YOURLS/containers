247f8ef840dd42cecb8ed767401f25ca51708418
Léo Colombaro <git@colombaro.fr>
2017-11-29 00:17:41 +0100

    rewrite docker setup
    prepare official package

---
+++ generate-stackbrew-library.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+set -eu
+
+declare -A aliases=(
+	[1.7.2]='1.7.2 latest'
+)
+
+self="$(basename "$BASH_SOURCE")"
+cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
+
+versions=( */ )
+versions=( "${versions[@]%/}" )
+
+# sort version numbers with highest first
+IFS=$'\n'; versions=( $(echo "${versions[*]}" | sort -rV) ); unset IFS
+
+# get the most recent commit which modified any of "$@"
+fileCommit() {
+	git log -1 --format='format:%H' HEAD -- "$@"
+}
+
+# get the most recent commit which modified "$1/Dockerfile" or any file COPY'd from "$1/Dockerfile"
+dirCommit() {
+	local dir="$1"; shift
+	(
+		cd "$dir"
+		fileCommit \
+			Dockerfile \
+			$(git show HEAD:./Dockerfile | awk '
+				toupper($1) == "COPY" {
+					for (i = 2; i < NF; i++) {
+						print $i
+					}
+				}
+			')
+	)
+}
+
+cat <<-EOH
+# this file is generated via https://github.com/YOURLS/docker-yourls/blob/$(fileCommit "$self")/$self
+
+Maintainers: YOURLS <team@yourls.org> (@YOURLS)
+GitRepo: https://github.com/YOURLS/docker-yourls.git
+EOH
+
+# prints "$2$1$3$1...$N"
+join() {
+	local sep="$1"; shift
+	local out; printf -v out "${sep//%/%%}%s" "$@"
+	echo "${out#$sep}"
+}
+
+for version in "${versions[@]}"; do
+	for variant in apache fpm; do
+		commit="$(dirCommit "$version/$variant")"
+
+		fullVersion="$(git show "$commit":"$version/$variant/Dockerfile" | awk '$1 == "ENV" && $2 == "YOURLS_VERSION" { print $3; exit }')"
+
+		versionAliases=()
+		while [ "$fullVersion" != "$version" -a "${fullVersion%[.-]*}" != "$fullVersion" ]; do
+			versionAliases+=( $fullVersion )
+			fullVersion="${fullVersion%[.-]*}"
+		done
+		versionAliases+=(
+			$version
+			${aliases[$version]:-}
+		)
+
+		variantAliases=( "${versionAliases[@]/%/-$variant}" )
+		variantAliases=( "${variantAliases[@]//latest-/}" )
+
+		if [ "$variant" = 'apache' ]; then
+			variantAliases+=( "${versionAliases[@]}" )
+		fi
+
+		echo
+		cat <<-EOE
+			Tags: $(join ', ' "${variantAliases[@]}")
+			GitCommit: $commit
+			Directory: $version/$variant
+		EOE
+	done
+done
---
+++ update.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+set -e
+
+declare -A cmd=(
+	[apache]='apache2-foreground'
+	[fpm]='php-fpm'
+)
+
+cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
+
+#current="$(curl -fsSL 'https://packagist.org/p/yourls/yourls.json' | jq -r '?')"
+current="$(curl -fsSL 'https://api.github.com/repos/YOURLS/YOURLS/releases' | jq -r '.[0].tag_name')"
+
+travisEnv=
+for variant in apache fpm; do
+    mkdir -p "$current/$variant"
+
+    cp Dockerfile.template "$current/$variant/Dockerfile"
+
+    sed -ri \
+        -e 's/%%VARIANT%%/'"$variant"'/' \
+        -e 's/%%VERSION%%/'"$current"'/' \
+        -e 's/%%CMD%%/'"${cmd[$variant]}"'/' \
+        "$current/$variant/Dockerfile"
+
+    if [ "$variant" = 'fpm' ]; then
+        sed -ri -e '/a2enmod/d' "$current/$variant/Dockerfile"
+    fi
+
+    cp -a docker-entrypoint.sh "$current/$variant/docker-entrypoint.sh"
+    cp -a config-docker.php "$current/$variant/config-docker.php"
+
+    travisEnv='\n  - VERSION='"$current"' VARIANT='"$variant$travisEnv"
+done
+
+travis="$(awk -v 'RS=\n\n' '$1 == "env:" { $0 = "env:'"$travisEnv"'" } { printf "%s%s", $0, RS }' .travis.yml)"
+printf "$travis"
+echo "$travis" > .travis.yml

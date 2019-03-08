c0190094e91011ebd111e4a8eb0f892a94ce564d
Léo Colombaro <git@colombaro.fr>
2019-03-08 20:34:07 +0100

    Fix shellcheck issues

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -18,7 +18,7 @@ if [[ "$1" == apache2* ]] || [ "$1" == php-fpm ]; then
 
 		: "${YOURLS_USER:=}"
 		: "${YOURLS_PASS:=}"
-		if [ "${YOURLS_USER}" -a "${YOURLS_PASS}" ]; then
+		if [ -n "${YOURLS_USER}" ] && [ -n "${YOURLS_PASS}" ]; then
 			sed -i "s/  getenv('YOURLS_USER') => getenv('YOURLS_PASS'),/  '${YOURLS_USER}' => '${YOURLS_PASS}',/g" /var/www/html/user/config.php
 		fi
 

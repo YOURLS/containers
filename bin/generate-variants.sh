4925877dbca56c1c5a57dffb887f24b65b5b1ee7
Léo Colombaro <git@colombaro.fr>
2021-03-19 21:54:50 +0100

    Move version usage into local file

--- bin/generate-variants.sh
+++ bin/generate-variants.sh
@@ -21,44 +21,41 @@ declare -A files=(
 
 cd "$(dirname "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")")"
 
-current="$1"
+version="$(head -n 1 yourls_version)"
 # Using Packagist API
-#if [ -z "$current" ]; then
-#	current="$(curl -fsSL 'https://packagist.org/p/yourls/yourls.json' | jq -r '.packages["yourls/yourls"]')"
+#if [ -z "$version" ]; then
+#	version="$(curl -fsSL 'https://packagist.org/p/yourls/yourls.json' | jq -r '.packages["yourls/yourls"]')"
 #fi
-#sha256="$(curl -fsSL 'https://packagist.org/p/yourls/yourls.json' | jq -r ".packages['yourls/yourls']['${current}'].dist.shasum")"
+#sha256="$(curl -fsSL 'https://packagist.org/p/yourls/yourls.json' | jq -r ".packages['yourls/yourls']['${version}'].dist.shasum")"
 
 # Using GitHub API
-if [ -z "$current" ]; then
-	current="$(curl -fsSL 'https://api.github.com/repos/YOURLS/YOURLS/releases/latest' | jq -r '.tag_name')"
+if [ -z "$version" ]; then
+	version="$(curl -fsSL 'https://api.github.com/repos/YOURLS/YOURLS/releases/latest' | jq -r '.tag_name')"
 fi
-sha256="$(curl -fsSL "https://github.com/YOURLS/YOURLS/archive/${current}.tar.gz" | sha256sum | awk '{ print $1 }')"
+sha256="$(curl -fsSL "https://github.com/YOURLS/YOURLS/archive/${version}.tar.gz" | sha256sum | awk '{ print $1 }')"
 
-base_folder="$2"
+baseFolder="$1"
 
 for variant in apache fpm fpm-alpine; do
-	mkdir -p "$base_folder$variant"
+	mkdir -p "$baseFolder$variant"
 
-	cp Dockerfile.template "$base_folder$variant/Dockerfile"
+	cp Dockerfile.template "$baseFolder$variant/Dockerfile"
 
 	sed -ri \
 		-e 's/%%VARIANT%%/'"$variant"'/' \
 		-e 's/%%VARIANT_EXTRAS%%/'"${extras[$variant]}"'/' \
 		-e 's/%%VARIANT_FILES%%/'"${files[$variant]}"'/' \
-		-e 's/%%VERSION%%/'"$current"'/' \
+		-e 's/%%VERSION%%/'"$version"'/' \
 		-e 's/%%SHA256%%/'"$sha256"'/' \
 		-e 's/%%CMD%%/'"${cmd[$variant]}"'/' \
 		"$base_folder$variant/Dockerfile"
 
-	cp -a docker-entrypoint.sh "$base_folder$variant/docker-entrypoint.sh"
-	cp -a config-docker.php "$base_folder$variant/config-docker.php"
+	cp -a docker-entrypoint.sh "$baseFolder$variant/docker-entrypoint.sh"
+	cp -a config-docker.php "$baseFolder$variant/config-docker.php"
 
 	if [ "$variant" = 'apache' ]; then
-		cp -a yourls.vhost "$base_folder$variant/.htaccess"
+		cp -a yourls.vhost "$baseFolder$variant/.htaccess"
 	fi
 
 	ciVariants="$variant${ciVariants:+, $ciVariants}"
 done
-
-sed -i "s/version:.*/version: [$current]/g" .github/workflows/ci.yml
-sed -i "s/variant:.*/variant: [$ciVariants]/g" .github/workflows/ci.yml

fbf9b9207180550c0808a0fa3b6ce7a330643ff5
Léo Colombaro <git@colombaro.fr>
2021-03-19 05:10:48 +0100

    Review scripting

--- bin/generate-variants.sh
+++ bin/generate-variants.sh
@@ -19,20 +19,27 @@ declare -A files=(
 	[fpm-alpine]=''
 )
 
-cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
+cd "$(dirname "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")")"
 
+current="$1"
 # Using Packagist API
-#current="$(curl -fsSL 'https://packagist.org/p/yourls/yourls.json' | jq -r '.packages["yourls/yourls"]')"
+#if [ -z "$current" ]; then
+#	current="$(curl -fsSL 'https://packagist.org/p/yourls/yourls.json' | jq -r '.packages["yourls/yourls"]')"
+#fi
 #sha256="$(curl -fsSL 'https://packagist.org/p/yourls/yourls.json' | jq -r ".packages['yourls/yourls']['${current}'].dist.shasum")"
 
 # Using GitHub API
-current="$(curl -fsSL 'https://api.github.com/repos/YOURLS/YOURLS/releases/latest' | jq -r '.tag_name')"
+if [ -z "$current" ]; then
+	current="$(curl -fsSL 'https://api.github.com/repos/YOURLS/YOURLS/releases/latest' | jq -r '.tag_name')"
+fi
 sha256="$(curl -fsSL "https://github.com/YOURLS/YOURLS/archive/${current}.tar.gz" | sha256sum | awk '{ print $1 }')"
 
-for variant in apache fpm fpm-alpine; do
-	mkdir -p "$variant"
+base_folder="${2:$2/}"
 
-	cp Dockerfile.template "$variant/Dockerfile"
+for variant in apache fpm fpm-alpine; do
+	mkdir -p "$base_folder$variant"
+
+	cp Dockerfile.template "$base_folder$variant/Dockerfile"
 
 	sed -ri \
 		-e 's/%%VARIANT%%/'"$variant"'/' \
@@ -41,13 +48,13 @@ for variant in apache fpm fpm-alpine; do
 		-e 's/%%VERSION%%/'"$current"'/' \
 		-e 's/%%SHA256%%/'"$sha256"'/' \
 		-e 's/%%CMD%%/'"${cmd[$variant]}"'/' \
-		"$variant/Dockerfile"
+		"$base_folder$variant/Dockerfile"
 
-	cp -a docker-entrypoint.sh "$variant/docker-entrypoint.sh"
-	cp -a config-docker.php "$variant/config-docker.php"
+	cp -a docker-entrypoint.sh "$base_folder$variant/docker-entrypoint.sh"
+	cp -a config-docker.php "$base_folder$variant/config-docker.php"
 
 	if [ "$variant" = 'apache' ]; then
-		cp -a yourls.vhost "$variant/.htaccess"
+		cp -a yourls.vhost "$base_folder$variant/.htaccess"
 	fi
 
 	ciVariants="$variant${ciVariants:+, $ciVariants}"
